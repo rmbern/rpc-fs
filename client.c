7fb9ebc22df59b9d6417952bf0f16e9d6977437b
RYAN BERNSTEIN <rmb280@libpc004>
2019-04-02 20:20:40 -0400

    THIS COMMIT IS UGLY AND BROKEN!!!!!
    Read and write are implemented WITHOUT mutex protection,
    and WITHOUT any checks that their return values are correct.

--- client.c
+++ client.c
@@ -11,59 +11,161 @@
 
 FileHandle ropen()
 {
-    static int sd = -2; // -1 reserved for sys error
-    if (sd == -2)
+  static int sd = -2; // -1 reserved for sys error
+  if (sd == -2)
+  {
+    struct addrinfo request;
+    memset(&request, 0, sizeof(struct sockaddr));
+    request.ai_flags = 0;
+    request.ai_family = AF_INET;
+    request.ai_socktype = SOCK_STREAM;
+    request.ai_protocol = 0;
+    request.ai_addrlen = 0;
+    request.ai_canonname = NULL;
+    request.ai_next = NULL;
+
+    struct addrinfo * responses;
+
+    int getaddr_result = getaddrinfo("localhost", 
+                                     "5000", 
+                                     &request,
+                                     &responses);
+    handle_getaddr_error(getaddr_result);
+
+    sd = socket(AF_INET, // Will use the network later
+                SOCK_STREAM, // TCP
+                0); // Use configured reasonable defaults 
+
+    if (sd == -1)
     {
-        struct addrinfo request;
-        memset(&request, 0, sizeof(struct sockaddr));
-        request.ai_flags = 0;
-        request.ai_family = AF_INET;
-        request.ai_socktype = SOCK_STREAM;
-        request.ai_protocol = 0;
-        request.ai_addrlen = 0;
-        request.ai_canonname = NULL;
-        request.ai_next = NULL;
-
-        struct addrinfo * responses;
-
-        int getaddr_result = getaddrinfo("localhost", 
-                                         "5000", 
-                                         &request,
-                                         &responses);
-        handle_getaddr_error(getaddr_result);
-
-        sd = socket(AF_INET, // Will use the network later
-                    SOCK_STREAM, // TCP
-                    0); // Use configured reasonable defaults 
-
-        if (sd == -1)
-        {
-            perror("Socket creation\n");
-            exit(1);
-        }
-
-        struct sockaddr address;
-        memset(&address, 0, sizeof(struct sockaddr));
-
-        if (connect(sd,
-                    responses->ai_addr, 
-                    sizeof(struct sockaddr)) == -1)
-        {
-            perror("Socket connect");
-            exit(1);
-        }
-        printf("CONNECTED\n");
-        // NOTE: CLIENT SENDS SERVER EOF WHEN THREAD DIES.
-        // TODO: IS THIS UB?
-        FileHandle fh;
-        fh.sd = sd;
-        fh.fd = NULL; 
-        
-        return fh;
+      perror("Socket creation\n");
+      exit(1);
     }
+
+    struct sockaddr address;
+    memset(&address, 0, sizeof(struct sockaddr));
+
+    if (connect(sd,
+                responses->ai_addr, 
+                sizeof(struct sockaddr)) == -1)
+    {
+      perror("Socket connect");
+      exit(1);
+    }
+    printf("CONNECTED\n");
+    // NOTE: CLIENT SENDS SERVER EOF WHEN THREAD DIES.
+    // TODO: IS THIS UB?
+    FileHandle fh;
+    fh.sd = sd;
+    fh.fd = NULL; 
+    
+    return fh;
+  }
+}
+
+int rread(FileHandle fh, void * buffer, int size)
+{
+  // TODO: TEST A TRUNCATION:
+  //       IE SOMETHING LIKE ASKING TO READ 1000 BYTES FROM
+  //       A 100 BYTE FILE
+ 
+  // Using fh socket, tell server to read size bytes
+  // from the remote file.
+  char command = 'R'; // R for Read
+  if (write(fh.sd, &command, sizeof(char)) < 0)
+  {
+    perror("Sending request for server to read file.");
+    exit(1);
+  }
+  if (write(fh.sd, &size, sizeof(int)) < 0)
+  {
+    perror("Informing server of how many bytes to read.");
+    exit(1);
+  }
+  // Server should send size consecutive bytes to the
+  // client.
+  
+  // Place these bytes into a file.
+  int bytes_read = read(fh.sd, buffer, size);
+  if (bytes_read < 0)
+  {
+    perror("Receiving file data from server.");
+    exit(1);
+  }
+
+  // NOTE: ALL API CALLS MUST CONTAIN THIS ERRNO READ.
+  // TODO: Abstract out to own function??
+  char err = -1;
+  if (read(fh.sd, &err, sizeof(char)) < 0)
+  {
+    perror("Reading errno from server");
+    exit(1);
+  }
+  
+  if (err)
+  {
+    fprintf(stderr, "Server returned errno %d\n", err);
+    exit(1);
+  }
+  return bytes_read;
+}
+
+int rwrite(FileHandle fh, void * buff, int size)
+{
+  // Using fh socket, tell server to read size bytes
+  // from the remote file.
+  char command = 'W'; // R for Read
+  if (write(fh.sd, &command, sizeof(char)) < 0)
+  {
+    perror("Sending request for server to write to file.");
+    exit(1);
+  }
+  if (write(fh.sd, &size, sizeof(int)) < 0)
+  {
+    perror("Informing server of how many bytes to write.");
+    exit(1);
+  }
+  // Server should write size consecutive bytes to file
+  int bytes_written = write(fh.sd, buff, size);
+  if (bytes_written < 0)
+  {
+    perror("Sending data for server to write to file.");
+    exit(1);
+  }
+
+  // NOTE: ALL API CALLS MUST CONTAIN THIS ERRNO READ.
+  // TODO: Abstract out to own function??
+  char err = -1;
+  if (read(fh.sd, &err, sizeof(char)) < 0)
+  {
+    perror("Reading errno from server");
+    exit(1);
+  }
+
+  if (err)
+  {
+    fprintf(stderr, "Server returned errno %d\n", err);
+    exit(1);
+  }
+  return bytes_written;
+
 }
 
 int main () {
   FileHandle fh = ropen();
-  sleep(600);
+
+  char buff[5];
+
+  // TEST CASE 1: BASIC READ
+  memset(buff, 0, 5);
+  int bytes = rread(fh, buff, 4);
+  printf("%d:%s\n", bytes, buff);
+  // END CASE 1
+
+  // TEST CASE 2: BASIC WRITE
+  memset(buff, 0, 5);
+  strncpy(buff, "POOP", 5);
+  bytes = rwrite(fh, buff, 4);
+  // END CASE 2
+  sleep(100);
 }
--- server.c
+++ server.c
@@ -1,176 +1,245 @@
 #include <sys/types.h> 
 #include <sys/socket.h> 
 #include <sys/un.h> 
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
 #include <unistd.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <errno.h>
 #include "common.h" 
 
 // TODO: SIGINT handler to close all fd's
 void * connection_thread(void * args)
 {
-    pthread_detach(pthread_self());
+  pthread_detach(pthread_self());
 
-    int sd = *(int*)args;
-    free(args); 
+  int sd = *(int*)args;
+  free(args); 
 
-    // Create server side file handle
-    // TODO: Manage globally for sighandler to clean up
-    //       Right now, we can lose data at the "tail
-    //       end" of our file buffer.
-    FILE * fd = fopen("TMP", "w");
-    if (fd == NULL)
+  // Create server side file handle
+  // TODO: Manage globally for sighandler to clean up
+  //     Right now, we can lose data at the "tail
+  //     end" of our file buffer.
+
+  int fd = open("TMP", O_APPEND | O_CREAT | O_RDWR);
+  if (fd == -1)
+  {
+    perror("File open\n");
+    exit(1);
+  }
+
+  char recieved_msg[100];
+  while(1)
+  {
+    memset(recieved_msg, 0, sizeof(recieved_msg));
+    char command_byte = -1;
+    int result = read(sd, &command_byte, 1);
+    if (result == 0)
     {
-      perror("File open\n");
+      printf("EOF or broken TCP connection detected.\n");
+      printf("Connection thread exiting\n");
+      close(sd);
+      close(fd);
+      pthread_exit(0);
+    }
+    else if (result < 0)
+    {
+      perror("Command Byte socket read\n");
+      goto ERROR;
+    } 
+    printf("%c\n", command_byte);
+    int recieved_size = -1;
+    int send_size = -1;
+    char err = 0;
+    char * buff;
+    switch(command_byte)
+    {
+      case 'R': // R for Read
+        result = read(sd, &recieved_size, sizeof(int));
+        if (result == 0)
+        {
+          err = errno;
+          printf("EOF or broken TCP connection detected.\n");
+          printf("Connection thread exiting\n");
+          close(sd);
+          close(fd);
+          pthread_exit(0);
+        }
+        else if (result < 0)
+        {
+          err = errno;
+          perror("Determining buffer size for read command\n");
+          goto ERROR;
+        }
+        buff = malloc(recieved_size);
+        memset(buff, 0, recieved_size);
+        // TODO: MUTEX PROTECT
+        if (read(fd, buff, recieved_size) < 0)
+        {
+          err = errno;
+          perror("File read.");
+          goto ERROR;
+        }
+        // END MUTEX PROTECT
+        if (write(sd, buff, recieved_size) < 0)
+        {
+          err = errno;
+          perror("Writing to client.");
+          goto ERROR;
+        }
+        free(buff);
+        break;
+      case 'W':
+        // TODO: abstract these reads out to fn?
+        result = read(sd, &recieved_size, sizeof(int));
+        if (result == 0)
+        {
+          err = errno;
+          printf("EOF or broken TCP connection detected.\n");
+          printf("Connection thread exiting\n");
+          close(sd);
+          close(fd);
+          pthread_exit(0);
+        }
+        else if (result < 0)
+        {
+          err = errno;
+          perror("Determining buffer size for read command\n");
+          goto ERROR;
+        }
+
+        buff = malloc(recieved_size);
+        memset(buff, 0, recieved_size);
+        result = read(sd, buff, recieved_size);
+        if (result == 0)
+        {
+          err = errno;
+          printf("EOF or broken TCP connection detected.\n");
+          printf("Connection thread exiting\n");
+          close(sd);
+          close(fd);
+          pthread_exit(0);
+        }
+        else if (result < 0)
+        {
+          err = errno;
+          perror("Determining buffer size for read command\n");
+          goto ERROR;
+        }
+        // TODO: MUTEX PROTECT
+        if (write(fd, buff, recieved_size) < 0)
+        {
+          err = errno;
+          perror("Writing to file\n");
+          goto ERROR;
+        }
+        // END MUTEX PROTECT
+        printf("WROTE %s TO FILE\n", buff);
+        break;
+      default:
+        fprintf(stderr, "BAD COMMAND RECIEVED FROM CLIENT\n");
+        break;
+    }
+
+    printf("%s\n", recieved_msg); 
+    // Send client server's errno at the end of 
+    // quick google search says errno is thread
+    // safe. Yay!
+ERROR:
+    if(write(sd, &err, sizeof(char)) < 0)
+    {
+      perror("Sending client errno");
       exit(1);
     }
-
-    char recieved_msg[100];
-    while(1)
+    if(err)
     {
-        memset(recieved_msg, 0, sizeof(recieved_msg));
-        char command_byte = -1;
-        int result = read(sd, &command_byte, 1);
-        if (result == 0)
-        {
-            printf("EOF or broken TCP connection detected.\n");
-            printf("Connection thread exiting\n");
-            break; // Go to thread cleanup
-        }
-        else if (result < 0)
-        {
-            perror("First socket read\n");
-            exit(1);
-        } 
-        printf("%c\n", command_byte);
-        if (command_byte != 'L' && command_byte != 'R')
-        {
-            fprintf(stderr, "Bad first value from client. Alerting client, and closing thread\n");
-            char byte = -1;
-            if (write(sd, &byte, 1) < 0)
-            {
-                perror("Socket write: bad direction value\n");
-                exit(1);
-            }
-            break; // Go to thread cleanup
-        }
-
-        result = read(sd, recieved_msg, sizeof(recieved_msg));
-        if (result == 0)
-        {
-            printf("EOF or broken TCP connection detected.\n");
-            printf("Connection thread exiting\n");
-            break; // Go to thread cleanup
-        }
-
-        else if (result < 0)
-        {
-            perror("Second socket read\n");
-            exit(1);
-        }
-
-        // notify client that there were no errors. 
-        char byte = 0;
-        if (write(sd, &byte, 1) < 0)
-        {
-            perror("Socket write\n");
-            exit(1);
-        }
-
-        if (write(sd, recieved_msg, sizeof(recieved_msg)) < 0)
-        {
-            perror("Socket write\n");
-            exit(1);
-        }
-        printf("%s\n", recieved_msg); 
+      exit(1);
     }
-
-    close(sd);
-    fclose(fd);
-    pthread_exit(0);
+  }
 }
 
 int main () 
 {
-    struct addrinfo request;
-    memset(&request, 0, sizeof(struct sockaddr));
-    request.ai_flags = AI_PASSIVE;
-    request.ai_family = AF_INET;
-    request.ai_socktype = SOCK_STREAM;
-    request.ai_protocol = 0;
-    request.ai_addrlen = 0;
-    request.ai_canonname = NULL;
-    request.ai_next = NULL;
+  struct addrinfo request;
+  memset(&request, 0, sizeof(struct sockaddr));
+  request.ai_flags = AI_PASSIVE;
+  request.ai_family = AF_INET;
+  request.ai_socktype = SOCK_STREAM;
+  request.ai_protocol = 0;
+  request.ai_addrlen = 0;
+  request.ai_canonname = NULL;
+  request.ai_next = NULL;
 
-    struct addrinfo * responses;
+  struct addrinfo * responses;
 
-    int getaddr_result = getaddrinfo(NULL, 
-                                     "5000", 
-                                     &request,
-                                     &responses);
-    handle_getaddr_error(getaddr_result);
+  int getaddr_result = getaddrinfo(NULL, 
+                                   "5000", 
+                                   &request,
+                                   &responses);
+  handle_getaddr_error(getaddr_result);
 
-    int sd = socket(AF_INET, 
-                    SOCK_STREAM, // TCP
-                    0); // Use configured reasonable defaults 
+  int sd = socket(AF_INET, 
+                  SOCK_STREAM, // TCP
+                  0); // Use configured reasonable defaults 
 
-    if (sd == -1)
+  if (sd == -1)
+  {
+    perror("Socket creation\n");
+    exit(1);
+  }
+  int on = 1;
+  if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == - 1)
+  {
+    perror("Socket configure\n");
+    exit(1);
+  }
+
+  struct sockaddr address;
+  memset(&address, 0, sizeof(struct sockaddr));
+
+  if (bind(sd,
+       responses->ai_addr, 
+       sizeof(struct sockaddr)) == -1)
+  {
+    perror("Socket bind\n");
+    exit(1);
+  }
+
+  if (listen(sd, 1) == -1)
+  {
+    perror("Socket listen\n");
+    exit(1);
+  }
+
+  // Accept connections continuously
+  while (1)
+  {
+    int sock_connection;
+    socklen_t * socklen = malloc(sizeof(socklen_t));
+    *socklen = responses->ai_addrlen;
+    sock_connection = accept(sd, responses->ai_addr, socklen);
+    if (sock_connection < 0)
     {
-        perror("Socket creation\n");
-        exit(1);
+      perror("Socket accept");
+      exit(1);
     }
-    int on = 1;
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == - 1)
+    free(socklen); 
+
+    int * args = malloc(sizeof(int));
+    *args = sock_connection;
+    pthread_t thread; // Not interested in thread id's so a single
+                      // pthread_t struct will suffice.
+    if (pthread_create(&thread, 0, connection_thread, args) == -1)
     {
-        perror("Socket configure\n");
-        exit(1);
+      perror("connection thread spawn\n"); 
+      exit(1);
     }
+  }
 
-    struct sockaddr address;
-    memset(&address, 0, sizeof(struct sockaddr));
-
-    if (bind(sd,
-             responses->ai_addr, 
-             sizeof(struct sockaddr)) == -1)
-    {
-        perror("Socket bind\n");
-        exit(1);
-    }
-
-    if (listen(sd, 1) == -1)
-    {
-        perror("Socket listen\n");
-        exit(1);
-    }
-
-    // Accept connections continuously
-    while (1)
-    {
-        int sock_connection;
-        socklen_t * socklen = malloc(sizeof(socklen_t));
-        *socklen = responses->ai_addrlen;
-        sock_connection = accept(sd, responses->ai_addr, socklen);
-        if (sock_connection < 0)
-        {
-            perror("Socket accept");
-            exit(1);
-        }
-        free(socklen); 
-
-        int * args = malloc(sizeof(int));
-        *args = sock_connection;
-        pthread_t thread; // Not interested in thread id's so a single
-                          // pthread_t struct will suffice.
-        if (pthread_create(&thread, 0, connection_thread, args) == -1)
-        {
-           perror("connection thread spawn\n"); 
-           exit(1);
-        }
-    }
-
-    freeaddrinfo(responses);
-    pthread_exit(0);
+  freeaddrinfo(responses);
+  pthread_exit(0);
 }
